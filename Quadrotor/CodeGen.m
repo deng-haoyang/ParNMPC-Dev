7f2a7c9e7e9fff1fba11a8d265e439a4e0656788
deng-haoyang <deng.haoyang.1993@gmail.com>
2020-07-30 15:51:11 +0900

    Add Simulink interface

--- Quadrotor/CodeGen.m
+++ Quadrotor/CodeGen.m
@@ -1,11 +1,34 @@
-% options
+%% options
 solverOptions = createSolverOptions();
 solverOptions.printLevel = 2;
 solverOptions.T      = 1;
 solverOptions.N      = 20;
-%
-codegenOptions = createCodegenOptions;
-codegenOptions.targetLang    = 'C';
+%% initial guess 
+% initial state
+x0 = [0,0,0,0,0,0,0,0,0].';
+
+% parameters
+yref = [0,0,0].';
+p  = zeros(OCP.dim.p,solverOptions.N);
+p(1:3,:) = repmat(yref,[1,solverOptions.N]);
+p(end,:)  = 0.1; % W
+% accurate initial guess
+solution = [];
+solution.u = [0,0,0,0,0].';
+solution.x = [x0,zeros(OCP.dim.x,1)];
+solution = solutionInterp(x0,p,solution);
+[solution,output] = NMPC_Solve(x0,p,solution,solverOptions);
+%%
+% generate soure files
+codegenOptions = createCodegenOptions();
+codegenOptions.targetLang    = 'C++';
+codegenOptions.cppNamespace  = false;
+
+% generate mex interface
 codegenOptions.MEX_buildMEXInterface = true;
+
+% generate simulink interface
+codegenOptions.solution_init = solution;
+codegenOptions.generateSimulinkInterface = true;
 
 NMPC_Solve_CodeGen(OCP,solverOptions,codegenOptions);
